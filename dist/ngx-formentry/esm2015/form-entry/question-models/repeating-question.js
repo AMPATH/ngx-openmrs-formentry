46f093f4aec6415224e86358de663d2fbe238837
maikofelix89 <maikofelix89@gmail.com>
2019-02-05 08:45:43 +0300

    revert to rxjs 6.3.3 due to 6.4.0 breaking changes

--- dist/ngx-formentry/esm2015/form-entry/question-models/repeating-question.js
+++ dist/ngx-formentry/esm2015/form-entry/question-models/repeating-question.js
@@ -1,6 +1,6 @@
 /**
  * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 import { AfeControlType } from '../../abstract-controls-extension/afe-control-type';
 import { NestedQuestion } from './interfaces/nested-questions';
