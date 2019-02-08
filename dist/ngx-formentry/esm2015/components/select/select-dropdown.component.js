abd841a1fed9b73ecdddfc1cdbaf9042326100f5
maikofelix89 <maikofelix89@gmail.com>
2019-02-08 11:28:55 +0300

    Bumped version 2.12.2

--- dist/ngx-formentry/esm2015/components/select/select-dropdown.component.js
+++ dist/ngx-formentry/esm2015/components/select/select-dropdown.component.js
@@ -1,6 +1,6 @@
 /**
  * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 import { Component, EventEmitter, Input, Output, ViewChild, ViewEncapsulation } from '@angular/core';
 import { STYLE } from './select-dropdown.component.style';
--- dist/ngx-formentry/esm2015/form-entry/directives/historical-value-directive.js
+++ dist/ngx-formentry/esm2015/form-entry/directives/historical-value-directive.js
@@ -1,6 +1,6 @@
 /**
  * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 import { Directive, HostListener, Input, Output, EventEmitter } from '@angular/core';
 import { HistoricalFieldHelperService } from '../helpers/historical-field-helper-service';
