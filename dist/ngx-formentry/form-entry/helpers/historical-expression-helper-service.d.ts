71dddc654a14b9c890e8a16b9967ae0c21d17803
derrick rono <derrickrono@gmail.com>
2023-07-17 16:45:35 +0300

    fix dist files

--- dist/ngx-formentry/form-entry/helpers/historical-expression-helper-service.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class HistoricalHelperService {
-    constructor();
-    evaluate(expr: string, dataSources: any, additionalScopevalues: any): any;
-    evaluatePrecondition(expr: string, dataSources: any, historicalValue: any): any;
-}
